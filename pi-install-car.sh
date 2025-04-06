13ca44a82bfad8521c5483d549b4778e3d0d6086
atomroflman <gpoweleit@gmail.com>
2025-04-06 20:07:59 +0200

    Add checkout to install script

--- pi-install-car.sh
+++ pi-install-car.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [! -f pi-install-base.sh]; then 
+    apt install git
+    git pull https://github.com/atomroflman/LteCar.git
+    cd LteCar
+fi
 
 bash pi-install-base.sh
--- pi-install-server.sh
+++ pi-install-server.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [! -f pi-install-base.sh]; then 
+    apt install git
+    git pull https://github.com/atomroflman/LteCar.git
+    cd LteCar
+fi
 
 bash pi-install-base.sh
 bash ./LteCar.Server/install.sh
