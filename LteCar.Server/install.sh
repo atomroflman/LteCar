2c891d0ed9ec60b4e2769786cc25181790c70289
atomroflman <gpoweleit@gmail.com>
2025-04-18 17:31:51 +0000

    Update install script

--- LteCar.Server/install.sh
+++ LteCar.Server/install.sh
@@ -3,3 +3,10 @@
 # Set current directory to the directory where the script is located
 cd "$(dirname "$0")"
 bash ./bash/install-janus.sh
+
+curl -sSL https://dot.net/v1/dotnet-install.sh >> ./bash/dotnet-install.sh
+bash ./bash/dotnet-install.sh
+
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools' >> ~/.bashrc
+source ~/.bashrc
