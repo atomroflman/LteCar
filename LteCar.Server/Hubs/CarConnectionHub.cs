1423bdef3607205ce4be3528e0270a5edd3c332c
atomroflman <gpoweleit@gmail.com>
2025-04-24 21:02:59 +0000

    ADD Video Stream server start

--- LteCar.Server/Hubs/CarConnectionHub.cs
+++ LteCar.Server/Hubs/CarConnectionHub.cs
@@ -4,24 +4,30 @@ using LteCar.Server;
 using LteCar.Shared;
 using Microsoft.AspNetCore.SignalR;
 
+namespace LteCar.Server.Hubs;
+
 public class CarConnectionHub : Hub<IConnectionHubClient>, ICarConnectionServer
 {
     public IConfiguration Configuration { get; }
+    public ILogger<CarConnectionHub> Logger { get; }
     private readonly CarConnectionStore _carConnectionStore;
 
-    public CarConnectionHub(CarConnectionStore carConnectionStore, IConfiguration configuration)
+    public CarConnectionHub(CarConnectionStore carConnectionStore, IConfiguration configuration, ILogger<CarConnectionHub> logger)
     {
         Configuration = configuration;
         _carConnectionStore = carConnectionStore;
+        Logger = logger;
     }
     
     public async Task<CarConfiguration> OpenCarConnection(string carId)
     {
+        Logger.LogInformation($"Open Car Connection: {carId}");
         if (_carConnectionStore.TryGetValue(carId, out var carConfiguration))
         {
             return carConfiguration.CarConfiguration;
         }
         var storagePath = Configuration.GetValue<string>("StoragePath");
+        Logger.LogDebug($"Storage Path: {storagePath}");
         var filePath = Path.Combine(storagePath, carId);
         CarConfiguration carConfig = null;
         if (File.Exists(filePath))
@@ -29,6 +35,7 @@ public class CarConnectionHub : Hub<IConnectionHubClient>, ICarConnectionServer
             var config = JsonSerializer.Deserialize<CarConfiguration>(File.ReadAllText(filePath));
             _carConnectionStore.Add(carId, new CarConnectionInfo() { CarConfiguration = config });
             carConfig = config;
+            Logger.LogDebug("Loaded car config...");
         }
         if (carConfig == null) 
         {
@@ -37,6 +44,7 @@ public class CarConnectionHub : Hub<IConnectionHubClient>, ICarConnectionServer
             var json = JsonSerializer.Serialize(carConfig);
             File.WriteAllText(filePath, json);
         }
+        
         if (carConfig.JanusConfiguration == null)
             carConfig.JanusConfiguration = new JanusConfiguration();
         carConfig.JanusConfiguration.JanusServerHost 
--- LteCar.Server/Program.cs
+++ LteCar.Server/Program.cs
@@ -1,21 +1,34 @@
-﻿using LteCar.Shared;
+﻿using LteCar.Server;
+using LteCar.Server.Hubs;
+using LteCar.Shared;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConsole();
+
+builder.Services.AddSingleton<VideoStreamRecieverService>();
+builder.Services.AddSingleton<CarConnectionStore>();
+
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Starting Janus service...");
+var vss = app.Services.GetRequiredService<VideoStreamRecieverService>();
+vss.RunVideoStreamServer();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 app.Use(async(ctx, next) => {
-    Console.WriteLine($"{ctx.Request.Method} {ctx.Request.Path}");
+    logger.LogDebug($"{ctx.Request.Method} {ctx.Request.Path}");
     await next();
+    logger.LogDebug($"{ctx.Response.StatusCode}");
 });
 
 app.UseStaticFiles(new StaticFileOptions()
@@ -30,7 +43,8 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
     endpoints.MapHub<CarConnectionHub>("/carconnectionhub");
+    endpoints.MapHub<CarControlHub>("/control");
+    endpoints.MapHub<TelemetryHub>("/telemetry");
 });
 
 app.Run();
-
