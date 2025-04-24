8fcc0cad0111366fd848ec10b993b44a0883421c
atomroflman <gpoweleit@gmail.com>
2025-04-24 22:51:28 +0200

    FIX: Configuration

--- LteCar.Onboard/Control/ControlExecutionService.cs
+++ LteCar.Onboard/Control/ControlExecutionService.cs
@@ -56,14 +56,3 @@ public class ControlExecutionService
     }
 }
 
-public class ChannelMap : Dictionary<string, ChannelMapItem>
-{
-}
-
-public class ChannelMapItem
-{
-    public int PhysicalGpio { get; set; }
-    public string ControlType { get; set; }
-    public Dictionary<string, object> Options { get; set; } = new();
-}
-
