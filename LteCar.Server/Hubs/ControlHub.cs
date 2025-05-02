ff0549ab9a28e784d8e13887c941f8eaff617c55
atomroflman <gpoweleit@gmail.com>
2025-05-02 15:06:26 +0200

    Control Basics up to Server Hub

--- LteCar.Server/Hubs/ControlHub.cs
+++ LteCar.Server/Hubs/ControlHub.cs
@@ -1,19 +1,32 @@
+using LteCar.Shared.HubClients;
 using Microsoft.AspNetCore.SignalR;
 
 namespace LteCar.Server.Hubs;
 
 public class CarControlHub : Hub<ICarControlClient>, ICarControlServer
 {
-    public void UpdateChannel(string conrtolId, string channelId, decimal value)    
+    public IConfiguration Configuration { get; }
+    private readonly CarConnectionStore _carConnectionStore;
+
+    public CarControlHub(CarConnectionStore carConnectionStore, IConfiguration configuration)
     {
-        
+        Configuration = configuration;
+        _carConnectionStore = carConnectionStore;
     }
-}
-
-public interface ICarControlServer
-{
-}
-
-public interface ICarControlClient
-{
+    
+    public async Task<string> AquireCarControl(string carId, string carSecret)
+    {
+        var session = await Clients.Client(carId).AquireCarControl(carSecret);
+        return session;
+    }
+    
+    public async Task ReleaseCarControl(string carId, string sessionId)
+    {
+        await Clients.Client(carId).ReleaseCarControl(sessionId);
+    }
+    
+    public async Task UpdateChannel(string carId, string sessionId, string channelId, decimal value)
+    {
+        await Clients.Client(carId).UpdateChannel(sessionId, channelId, value);
+    }
 }
