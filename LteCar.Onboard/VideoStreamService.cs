8a1a2ceaeff48b736ad882590962c00a99565bc2
atomroflman <gpoweleit@gmail.com>
2025-05-02 15:03:35 +0200

    UPD Video Service

--- LteCar.Onboard/VideoStreamService.cs
+++ LteCar.Onboard/VideoStreamService.cs
@@ -1,12 +1,66 @@
 using System.Diagnostics;
-
+x^
 namespace LteCar.Onboard;
+
+public class VideoSettings
+{
+    public static VideoSettings Default => new VideoSettings
+    {
+        Width = 640,
+        Height = 480,
+        Framerate = 30
+    };
+
+    public int? Width { get; set; }
+    public int? Height { get; set; }
+    public int? Framerate { get; set; }
+}
 
 public class VideoStreamService
 {
+    private VideoSettings _videoSettings;
+    private Process _libcameraProcess;
+
+    public void UpdateCameraSettings(VideoSettings newSettings)
+    {
+        var hasChanges = false;
+        foreach (var prop in typeof(VideoSettings).GetProperties())
+        {
+            var newValue = prop.GetValue(newSettings);
+            var oldValue = prop.GetValue(_videoSettings);
+
+            if (newValue == null)
+                continue;
+            
+            if (!newValue.Equals(oldValue))
+            {
+                Console.WriteLine($"Updating {prop.Name} from {oldValue} to {newValue}");
+                prop.SetValue(_videoSettings, newValue);
+                hasChanges = true;
+            }
+        }
+        if (!hasChanges)
+        {
+            Console.WriteLine("No changes detected in camera settings.");
+            return;
+        }
+        // Update the camera settings
+        _videoSettings = newSettings;
+        StartLibcameraProcess();
+
+    }
+
     public void StartLibcameraProcess()
     {
-        // Start the libcamera process
+        // Kill the existing process if it's running
+        if (_libcameraProcess != null && !_libcameraProcess.HasExited)
+        {
+            _libcameraProcess.Kill();
+            _libcameraProcess.Dispose();
+            _libcameraProcess = null;
+            Console.WriteLine("Libcamera process killed.");
+        }
+
         var process = new Process();
         process.StartInfo.FileName = "libcamera-vid";
         process.StartInfo.Arguments = "--width 640 --height 480 --framerate 30 --inline --output -";
@@ -14,7 +68,8 @@ public class VideoStreamService
         process.StartInfo.RedirectStandardOutput = true;
         process.StartInfo.RedirectStandardError = true;
         process.Start();
-
+        _libcameraProcess = process;
+        Console.WriteLine("Libcamera process started with PID: " + process.Id);
         // Read the output stream
         Task.Run(() =>
         {
