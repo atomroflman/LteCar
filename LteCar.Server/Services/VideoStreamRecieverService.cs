8a6bfccf3025424790fb454479a49c3b4358123f
atomroflman <gpoweleit@gmail.com>
2025-05-02 15:06:26 +0200

    ADD Video Stream server start

--- LteCar.Server/Services/VideoStreamRecieverService.cs
+++ LteCar.Server/Services/VideoStreamRecieverService.cs
@@ -1,9 +1,31 @@
+using System.Diagnostics;
+
 namespace LteCar.Server;
 
 public class VideoStreamRecieverService
 {
+    private Process? _janusProcess;
+
+    public VideoStreamRecieverService(ILogger<VideoStreamRecieverService> logger)
+    {
+        Logger = logger;
+    }
+
+    public ILogger<VideoStreamRecieverService> Logger { get; }
+
     public void RunVideoStreamServer()
     {
-        
+        var startParams = new ProcessStartInfo("/opt/janus/bin/janus") {
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        };
+        _janusProcess = new Process();
+        _janusProcess.OutputDataReceived += (obj, e) => {
+            Logger.LogInformation(e.Data);
+        };
+        _janusProcess.StartInfo = startParams;
+        _janusProcess.Start();
     }
 }
