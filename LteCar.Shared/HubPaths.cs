2cdc3b1c3e08d05cc03016785e703cdf94c43ec1
atomroflman <gpoweleit@gmail.com>
2025-05-12 21:32:38 +0000

    ADD: HubPath shared constants

---
+++ LteCar.Shared/HubPaths.cs
@@ -0,0 +1,7 @@
+public class HubPaths
+{
+    public const string CarConnectionHub = "/hubs/connection";
+    public const string CarControlHub = "/hubs/control";
+    public const string TelemetryHub = "/hubs/telemetry";
+    public const string CarUiHub = "/hubs/carui";
+}
