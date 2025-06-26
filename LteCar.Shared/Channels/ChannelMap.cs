7dab7e4815868508162f2a4b832042641c2b9156
atomroflman <gpoweleit@gmail.com>
2025-06-26 20:46:38 +0000

    ADD Channel Manager

--- LteCar.Shared/Channels/ChannelMap.cs
+++ LteCar.Shared/Channels/ChannelMap.cs
@@ -4,9 +4,18 @@ namespace LteCar.Shared.Channels;
 
 public class ChannelMap
 {
+    [JsonPropertyName("pinManagers")]
+    public Dictionary<string, PinManagerMapItem> PinManagers { get; set; } = new();
     [JsonPropertyName("controlChannels")]
     public Dictionary<string, ControlChannelMapItem> ControlChannels { get; set; } = new();
     [JsonPropertyName("telemetryChannels")]
     public Dictionary<string, TelemetryChannelMapItem> TelemetryChannels { get; set; } = new();
 }
 
+public class PinManagerMapItem
+{
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+    [JsonPropertyName("options")]
+    public Dictionary<string, string> Options { get; set; } = new();
+}
