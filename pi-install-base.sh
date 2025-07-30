cbec0ae48e7ac1fb71ebf37a85a9639ad0b3259e
atomroflman <gpoweleit@gmail.com>
2025-07-30 20:17:00 +0200

    FIX Install scripts

--- pi-install-base.sh
+++ pi-install-base.sh
@@ -3,6 +3,7 @@
 # This script installs the shared packages for a Raspberry Pi setup.
 # It is required to run this script with root privileges.
 # Check if the script is run as root
+echo "Running pi-install-base.sh"
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
@@ -11,6 +12,11 @@ fi
 apt update
 apt upgrade
 apt install git nano -y
+
+echo "Installing dotnet..."
+sudo -u "$SUDO_USER" bash -c 'curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0'
+sudo -u "$SUDO_USER" bash -c "echo 'export DOTNET_ROOT=\$HOME/.dotnet' >> \$HOME/.bashrc"
+sudo -u "$SUDO_USER" bash -c "echo 'export PATH=\$PATH:\$HOME/.dotnet:\$HOME/.dotnet/tools' >> \$HOME/.bashrc"
 
 # Check if we are already inside a git repo
 if [ -d ".git" ]; then
