ddc1f3d6ae805c6611cc9795062c99ebb9da9a47
atomroflman <gpoweleit@gmail.com>
2025-11-23 14:01:13 +0100

    Updated Truck scripts

--- Builds/PropagandaTruck/all-video-playlist.sh
+++ Builds/PropagandaTruck/all-video-playlist.sh
@@ -2,14 +2,14 @@
 
 # Verzeichnisse
 VIDEO_DIR="$HOME/Videos"
-ls "$HOME/Video"
+ls "$VIDEO_DIR"
 SOCKETS=("/tmp/mpvsocket0" "/tmp/mpvsocket1")
-
 printf '{ "command": ["set_property", "audio-device", "alsa/hdmi:CARD=vc4hdmi0,DEV=0"] }\n' | socat - UNIX-CONNECT:/tmp/mpvsocket0
+printf '{ "command": ["set_property", "audio-device", "alsa/hdmi:CARD=vc4hdmi1,DEV=0"] }\n' | socat - UNIX-CONNECT:/tmp/mpvsocket1
 
 # 1. Volume auf 100 setzen
 for sock in "${SOCKETS[@]}"; do
-    printf '{ "command": ["set_property", "volume", 100] }\n' | socat - UNIX-CONNECT:"$sock"
+    printf '{ "command": ["set_property", "volume", 110] }\n' | socat - UNIX-CONNECT:"$sock"
 done
 
 # 2. Alle Videos in Playlist laden
@@ -24,4 +24,5 @@ done
 # 3. Ab dem ersten Video abspielen
 for sock in "${SOCKETS[@]}"; do
     printf '{ "command": ["playlist-play-index", 0] }\n' | socat - UNIX-CONNECT:"$sock"
+    printf '{ "command": ["set_property", "pause", false] }\n' | socat - UNIX-CONNECT:"$sock"
 done
