b1bcbb6371a2ff31898f39538f6d419d0c99ca5e
atomroflman <gpoweleit@gmail.com>
2026-06-23 00:16:51 +0200

    Fix: adapter.js script tag muss absolut sein
    Bei Aufruf auf Subrouten wie /car/2 wurde der relative Pfad
    adapter.js zu /car/adapter.js aufgeloest, was von Next.js als
    SPA-Route mit 200 + index.html beantwortet wurde. Browser
    bekam HTML statt JavaScript, adapter.js wurde nie geladen.
    
    janus.js in video-stream.tsx hatte das schon richtig (/janus.js).

--- Client/src/pages/_document.tsx
+++ Client/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="de">
     <Head>
-      <script src="adapter.js" />
+      <script src="/adapter.js" />
     </Head>
       <body className="antialiased">      
         <Main />
